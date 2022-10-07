b5db14a072c4dbe657e8c1b95401be8ef4e013b9
fstawicki <stawickifilip1@gmail.com>
2022-10-07 15:13:42 +0200

    added cell component

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
-import Gabinet from './Gabinet'
 
 import '../styles/Calendar.scss';
 
 const Calendar = () => {
   return (
     <div className='calendar'>
-      <h1 className="date">DATA</h1>
-      <div className="gabinetsContainer">
-        <Gabinet gabinetName={'Gabinet 1'} />
-      </div>
+      <h1>Today is: </h1>
+
     </div>
   )
 }
---
+++ src/components/Cell.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cell = (props) => {
+  return (
+    <div>${props.children}</div>
+  )
+}
+
+export default Cell;
