2e58512a4722f95191231637901f814b4e956eac
fstawicki <stawickifilip1@gmail.com>
2022-10-14 20:32:56 +0200

    working on calendar - shows current month, added prefix and suffix days to improve looks

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,33 +1,63 @@
 import React from 'react';
 import { useState } from 'react';
-import { startOfMonth, endOfMonth, differenceInDays } from 'date-fns'
+import { startOfMonth, endOfMonth, differenceInDays, subMonths, addMonths } from 'date-fns'
 import '../styles/Calendar.scss';
 
 const daysOfWeek = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 const Calendar = () => {
 
   const [today, setToday] = useState(new Date());
-  const startDate = startOfMonth(new Date());
-  const endDate = endOfMonth(new Date());
-  const numDays = (differenceInDays(endDate, startDate) + 1);
+  const [startDate, setStartDate] = useState(startOfMonth(new Date()));
+  const [navDateMonth, setNavDateMonth] = useState(months[new Date().getMonth()]);
+  const [navDateYear, setNavDateYear] = useState(new Date().getFullYear());
+  const [endDate, setEndDate] = useState(endOfMonth(new Date()));
+  const [numDays, setNumDays] = useState((differenceInDays(endDate, startDate) + 1));
+
+  const [prefixDays, setPrefixDays] = useState(startDate.getDay() - 1);
+  // minus one adapts function for mon - sun calendar instead of sun - sat
+  const [sufixDays, setSufixDays] = useState(7 - endDate.getDay());
+
+
+  console.log(startDate)
+
+
+  const goToPrevMonth = () => {
+    setStartDate(startOfMonth(new Date() -1));
+    console.log(startDate)
+  }
+
+  const goToNextMonth = () => {
+
+  }
+  console.log(startDate)
 
   return (
     <div className='calendar'>
       <div className="calendar_nav">
-        <div className="calendar_cell skip">{'<<'}</div>
-        <div className="calendar_cell skip">{'<'}</div>
-        <div className='calendar_cell date'>October 2022</div>
-        <div className="calendar_cell skip">{'>'}</div>
-        <div className="calendar_cell skip">{'>>'}</div>
+        <div className="calendar_cell skip">{''}</div>
+        <div onClick={goToPrevMonth} className="calendar_cell skip">{'<'}</div>
+        <div className='calendar_cell date'>{navDateMonth} {navDateYear}</div>
+        <div onClick={goToNextMonth} className="calendar_cell skip">{'>'}</div>
+        <div className="calendar_cell skip">{''}</div>
+
         {daysOfWeek.map((day) =>(
           <div key={day} className="calendar_cell dayName">{day}</div>
         ))}
-        {
-          Array.from({length: numDays}).map((_, index) => (
+
+        {Array.from({length: prefixDays}).map((_, index) => (
+          <div key={index} className="calendar_cell" />
+        ))}
+
+        {Array.from({length: numDays}).map((_, index) => (
             <div key={index} className="calendar_cell">{index + 1}</div>
-          ))
-        }
+        ))}
+
+        {Array.from({length: sufixDays}).map((_, index) => (
+          <div key={index} className="calendar_cell" />
+        ))}
+
       </div>
     </div>
   )
